7d9126545ad76b709fdaf55e7c2f809a8cd443e6
weisintai <wstai1214@gmail.com>
2024-08-09 14:11:59 +0800

    Implement useAuth hook

--- src/lyra_frontend/src/main.jsx
+++ src/lyra_frontend/src/main.jsx
@@ -1,9 +1,13 @@
-import React, { StrictMode } from "react";
+import React, { StrictMode, createContext, useContext } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { useAuth, AuthProvider, AuthContext } from "./hooks/useAuth";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
+
+import { idlFactory, canisterId } from "../../declarations/lyra_backend";
+import { useAuthClient } from "./lib/use-auth-client";
 
 // Create a new router instance
 const router = createRouter({ routeTree });
@@ -11,11 +15,19 @@ const router = createRouter({ routeTree });
 // Render the app
 const rootElement = document.getElementById("root");
 
+function App() {
+  const isAuthenticated = useAuth().isAuthenticated;
+
+  return <RouterProvider router={router} context={{ isAuthenticated }} />;
+}
+
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </StrictMode>
   );
 }
--- src/lyra_frontend/src/routes/__root.jsx
+++ src/lyra_frontend/src/routes/__root.jsx
@@ -5,11 +5,11 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
+        <Link to="/`" className="[&.active]:font-bold">
+          Contact
+        </Link>
+        <Link to="/dashboard" className="[&.active]:font-bold">
+          Login
         </Link>
       </div>
       <hr />
